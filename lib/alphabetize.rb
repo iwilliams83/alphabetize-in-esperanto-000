e45e846749f3cd4227093437985c50c284a80e91
iwilliams83 <indirajamurath@gmail.com>
2017-11-13 23:26:42 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
   # code here
+  #arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
+  arr.sort_by do |word|
+      esp_alphabet.index(word[i])
+    end
+    
 end
